b445ac65800adc835edffb4b50bac872adb825a4
Abdelrahman Abuhasan <abdabhasan@gmail.com>
2024-03-24 15:40:30 +0300

    used TS for admin panel

---
+++ src/components/Containers/AdminPageContainers/AdminUserForm.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { addUser } from "@/lib/actions";
+import { useFormState } from "react-dom";
+
+type Props = {};
+
+const AdminUserForm: React.FC = (props: Props) => {
+  const [state, formAction] = useFormState(addUser, undefined);
+
+  return (
+    <form action={formAction} className="flex flex-col gap-5">
+      <h1>Add New User</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="username"
+        className="p-5 bg-gray-200 border-none rounded-md text-gray-700"
+      />
+      <input
+        type="text"
+        name="email"
+        placeholder="email"
+        className="p-5 bg-gray-200 border-none rounded-md text-gray-700"
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="password"
+        className="p-5 bg-gray-200 border-none rounded-md text-gray-700"
+      />
+      <input
+        type="text"
+        name="img"
+        placeholder="img"
+        className="p-5 bg-gray-200 border-none rounded-md text-gray-700"
+      />
+      <select
+        name="isAdmin"
+        className="p-5 bg-gray-200 border-none rounded-md text-gray-700"
+      >
+        <option value="false">Is Admin?</option>
+        <option value="false">No</option>
+        <option value="true">Yes</option>
+      </select>
+      <button className="p-5 cursor-pointer bg-blue-500 border-none rounded-md text-gray-700 font-bold">
+        Add
+      </button>
+      {state?.error}
+    </form>
+  );
+};
+
+export default AdminUserForm;
---
+++ src/components/Containers/AdminPageContainers/AdminUsers.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import { deleteUser } from "@/lib/actions";
+import axios from "axios";
+
+type Props = {};
+
+const USERS_ENDPOINT = `${process.env.API_ENDPOINT}/user`;
+
+// GET users from api
+const getData = async () => {
+  try {
+    const res = await axios.get(USERS_ENDPOINT);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const AdminUsers: React.FC = async (props: Props) => {
+  const users = await getData();
+
+  return (
+    <div className="flex flex-col gap-5">
+      <h1>Users</h1>
+      {users?.map((user) => (
+        <div
+          className="my-5 flex items-center justify-between gap-5"
+          key={user.id}
+        >
+          <div className="flex items-center gap-5">
+            <Image
+              src={user.img || "/noAvatar.png"}
+              alt=""
+              width={50}
+              height={50}
+            />
+            <span>{user.username}</span>
+          </div>
+          <form action={deleteUser}>
+            <input type="hidden" name="id" value={user.id} />
+            <button className="px-2.5 py-1 bg-red-600/60 text-white rounded-md cursor-pointer">
+              Delete
+            </button>
+          </form>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AdminUsers;
